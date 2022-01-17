5938a0f18f7e4d70e62d22c1827d3c6337a1482d
Heloisa Magalhães <heloisaalmeida99@gmail.com>
2022-01-16 21:21:57 -0300

    first commit

---
+++ 06-operadores-matematicos/index.js
@@ -0,0 +1,8 @@
+let primeiroValor = 4 
+let segundoValor = 2
+
+console.log(primeiroValor + segundoValor) // adição
+console.log(primeiroValor - segundoValor) // subtração
+console.log(primeiroValor / segundoValor) // divisão
+console.log(primeiroValor * segundoValor) // multiplicação
+console.log(primeiroValor % segundoValor) //quero o resto dessa divisão
