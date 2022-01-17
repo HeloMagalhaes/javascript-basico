6f1a6c9528ce6c483e5433d7c6651d9f30878509
Heloisa Magalhães <heloisaalmeida99@gmail.com>
2022-01-16 21:20:35 -0300

    first commit

---
+++ 04-tipos-primitivos/index.js
@@ -0,0 +1,11 @@
+let nome ='Roberto'
+let numero = 1230
+let usuarioVerificado = false
+
+// if(usuarioVerificado){
+//     console.log("usuário verificado")
+// }
+// else{
+//     console.log("usuário não é verificado")
+// }
+let pessoa = null 
